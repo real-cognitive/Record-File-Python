a13c30f55bd20c21c04ea6ffa60555f97a3f368b
Arnav Gupta <arnavg802@gmail.com>
2026-01-31 20:24:23 +0530

    cleaning

--- Project/test.py
+++
@@ -1,7 +0,0 @@
-# create table playlist(
-#     pid int NOT NULL AUTO_INCREMENT,
-#     Name varchar(40),
-#     Playlist_data text not null,
-#     uid int,
-#     PRIMARY KEY(pid),
-#     FOREIGN KEY(uid) references auth(id));
